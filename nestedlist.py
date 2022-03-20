00382d5cc3b0494282a86391cc3d03387cd32f25
manolis <manoliszaxa@gmail.com>
2022-03-20 18:44:08 +0200

    glukoulikes ekfraseis

--- nestedlist.py
+++ nestedlist.py
@@ -1,8 +1,8 @@
-x=int(input("dwse arithmo eggrafwn:"))
+x=int(input("dwse arithmako eggrafwn:"))
 l=[]
 for i in range(0,x):
-    n=str(input("dwse onoma mathiti:"))
-    b=int(input("dwse bathmo mathiti"))
+    n=str(input("dwse onomataki mathitou:"))
+    b=int(input("dwse bathmo mathitou"))
     l1=[]
     l1.append(n)
     l1.append(b)
